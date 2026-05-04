378d39f00a8a6c7180bba38663c92bdf13b3824d
Fabio Aguiar <fabinhobarreto2@gmail.com>
2026-05-04 20:34:07 -0300

    Estilizando valores de balance-card

--- src/app/features/home/components/balance-card/balance-card.ts
+++ src/app/features/home/components/balance-card/balance-card.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 
 @Component({
@@ -8,7 +8,18 @@ import { MatCardModule } from '@angular/material/card';
   styleUrl: './balance-card.scss',
 })
 export class BalanceCard {
-  type = input.required<'income' | ' outcome' | 'balance'>()
+  type = input.required<'income' | 'outcome' | 'balance'>()
   label = input.required<string>()
   value = input.required<number>()
+
+  cssClass = computed(() => {
+    switch (this.type()) {
+      case 'income':
+        return 'income';
+      case 'outcome':
+        return 'outcome';
+      case 'balance':
+        return this.value() > 0 ? 'income' : 'outcome';
+    }
+  })
 }
