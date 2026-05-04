a38ecc9b904ab331a64650205fa39fd0be206318
Fabio Aguiar <fabinhobarreto2@gmail.com>
2026-05-04 08:39:23 -0300

    Criando componente para mostrar valores

---
+++ src/app/features/home/home.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { BalanceCard } from './components/balance-card/balance-card';
+
+@Component({
+  selector: 'app-home',
+  imports: [BalanceCard],
+  templateUrl: './home.html',
+  styleUrl: './home.scss',
+})
+export class Home {
+
+}
