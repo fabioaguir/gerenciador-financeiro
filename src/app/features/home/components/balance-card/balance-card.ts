8a49b6a0e7235e05ecbf78801ff406cee404de3a
Fabio Aguiar <fabinhobarreto2@gmail.com>
2026-05-04 20:46:13 -0300

    Melhorando tipagem de balance-card

--- src/app/features/home/components/balance-card/balance-card.ts
+++ src/app/features/home/components/balance-card/balance-card.ts
@@ -1,6 +1,13 @@
 import { Component, computed, input } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 
+type BalanceCardType = 'income' | 'outcome' | 'balance';
+
+enum ValueCssClass {
+  income = 'income',
+  outcome = 'outcome',
+}
+
 @Component({
   selector: 'app-balance-card',
   imports: [MatCardModule],
@@ -8,18 +15,18 @@ import { MatCardModule } from '@angular/material/card';
   styleUrl: './balance-card.scss',
 })
 export class BalanceCard {
-  type = input.required<'income' | 'outcome' | 'balance'>()
+  type = input.required<BalanceCardType>()
   label = input.required<string>()
   value = input.required<number>()
 
-  cssClass = computed(() => {
+  cssClass = computed<ValueCssClass>(() => {
     switch (this.type()) {
       case 'income':
-        return 'income';
+        return ValueCssClass.income;
       case 'outcome':
-        return 'outcome';
+        return ValueCssClass.outcome;
       case 'balance':
-        return this.value() > 0 ? 'income' : 'outcome';
+        return this.value() > 0 ? ValueCssClass.income : ValueCssClass.outcome;
     }
   })
 }
