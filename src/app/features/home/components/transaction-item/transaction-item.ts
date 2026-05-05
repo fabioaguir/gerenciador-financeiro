e9f0d815c46415577b572a2e2603977b99f5e911
Fabio Aguiar <fabinhobarreto2@gmail.com>
2026-05-04 22:46:13 -0300

    Criando componente para renderizar o valor da transação

--- src/app/features/home/components/transaction-item/transaction-item.ts
+++ src/app/features/home/components/transaction-item/transaction-item.ts
@@ -2,10 +2,12 @@ import { Component, input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { Transaction } from '../../../../shared/transaction/intafaces/transaction';
+import { TransactionValue } from "./components/transaction-value/transaction-value";
+
 
 @Component({
   selector: 'app-transaction-item',
-  imports: [MatCardModule, MatButtonModule],
+  imports: [MatCardModule, MatButtonModule, TransactionValue],
   templateUrl: './transaction-item.html',
   styleUrl: './transaction-item.scss',
 })
