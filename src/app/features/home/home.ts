59fdbd02b5678cbb827d4b842104cb3113c3b53f
Fabio Aguiar <fabinhobarreto2@gmail.com>
2026-05-05 20:17:09 -0300

    Mostrando mensagem quando não há transações

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -3,31 +3,32 @@ import { Balance } from "./components/balance/balance";
 import { TransactionItem } from "./components/transaction-item/transaction-item";
 import { Transaction } from '../../shared/transaction/intafaces/transaction';
 import { TransactionType } from '../../shared/transaction/enuns/transaction-type';
+import { NoTransaction } from "./components/no-transaction/no-transaction";
 
 @Component({
   selector: 'app-home',
-  imports: [Balance, TransactionItem],
+  imports: [Balance, TransactionItem, NoTransaction],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 export class Home {
 
   transactions = signal<Transaction[]>([
-    {
-      value: 100, type: TransactionType.INCOME,
-      title: 'Salário'
-    },
-    {
-      value: 50, type: TransactionType.OUTCOME,
-      title: 'Aluguel'
-    },
-    {
-      value: 50, type: TransactionType.INCOME,
-      title: 'Freelance'
-    },
-    {
-      value: 100, type: TransactionType.OUTCOME,
-      title: 'Compras'
-    },
+    // {
+    //   value: 100, type: TransactionType.INCOME,
+    //   title: 'Salário'
+    // },
+    // {
+    //   value: 50, type: TransactionType.OUTCOME,
+    //   title: 'Aluguel'
+    // },
+    // {
+    //   value: 50, type: TransactionType.INCOME,
+    //   title: 'Freelance'
+    // },
+    // {
+    //   value: 100, type: TransactionType.OUTCOME,
+    //   title: 'Compras'
+    // },
   ])
 }
