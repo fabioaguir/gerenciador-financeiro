b5374bed7b8a0eb01ebeb8ebf4f908be3c8f86bf
Fabio Aguiar <fabinhobarreto2@gmail.com>
2026-05-12 15:37:09 -0300

    feat: store para gerenciar a sessão do usuário

---
+++ src/app/core/auth/stores/logged-in-user-store.service.ts
@@ -0,0 +1,23 @@
+import { computed, Injectable, signal } from '@angular/core';
+import { User } from '../interface/user';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoggedInUserStoreService {
+
+  private readonly state = signal<User | null>(null);
+
+  currentUser = computed(() => this.state());
+
+  isLoggedIn = computed(() => !!this.state());
+
+  setUser(user: User) {
+    this.state.set(user);
+  }
+
+  logout() {
+    this.state.set(null);
+  }
+
+}
