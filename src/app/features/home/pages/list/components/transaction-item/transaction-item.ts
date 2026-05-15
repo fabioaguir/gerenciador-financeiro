c60f17d7ab6f5d54f29ae3ba67ad6a633f6cb3d4
Fabio Aguiar <fabinhobarreto2@gmail.com>
2026-05-14 21:54:28 -0300

    feat: Criando diretiva para abstrair classe CSS

--- src/app/features/home/pages/list/components/transaction-item/transaction-item.ts
+++ src/app/features/home/pages/list/components/transaction-item/transaction-item.ts
@@ -3,11 +3,12 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { TransactionValue } from "./components/transaction-value/transaction-value";
 import { Transaction } from '../../../../../../shared/transaction/intafaces/transaction';
+import { CustomColorDirective } from '@shared/material/buttons/directive/custom-color.directive';
 
 
 @Component({
   selector: 'app-transaction-item',
-  imports: [MatCardModule, MatButtonModule, TransactionValue],
+  imports: [MatCardModule, MatButtonModule, TransactionValue, CustomColorDirective],
   templateUrl: './transaction-item.html',
   styleUrl: './transaction-item.scss',
 })
---
+++ src/app/shared/material/buttons/directive/custom-color.directive.ts
@@ -0,0 +1,26 @@
+import { Directive, effect, inject, input, ElementRef, Renderer2 } from '@angular/core';
+
+
+type ColorType = 'error';
+
+@Directive({
+  selector: '[matButton]'
+})
+export class CustomColorDirective {
+
+  elementRef = inject(ElementRef);
+  render2 = inject(Renderer2);
+
+  color = input<ColorType>(undefined, { alias: 'matButtonColor' });
+
+  constructor() {
+    effect(() => {
+      if (this.color()) {
+
+        this.render2.addClass(this.elementRef.nativeElement, `button-${this.color()}`);
+        console.log(this.color());
+      }
+    });
+  }
+
+}
